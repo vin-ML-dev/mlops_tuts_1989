cb5c014596bf56680d5fe46551f26bd5c23a3c3a
vin-ML-dev <vinreena15@gmail.com>
2024-05-27 23:53:37 +0530

    mlflow basics

---
+++ 4_ml_flow/mlflow_basics1.py
@@ -0,0 +1,33 @@
+import argparse
+import time
+import mlflow,os
+
+def eval_metric(p1,p2):
+    output_metric = p1**2+p2**2
+    return output_metric
+
+def main(inp1,inp2):
+    mlflow.set_experiment("demo_Experiment")
+    #with mlflow.start_run(run_name='Example Demo'):
+    with mlflow.start_run(run_name='Example Demo'):
+        mlflow.set_tag("version","1.0.0")
+        mlflow.log_param("param1",inp1)
+        mlflow.log_param("param2",inp2)
+
+        metric = eval_metric(p1=inp1,p2=inp2)
+        mlflow.log_metric("eval_metric",metric)
+
+        os.makedirs("dummy",exist_ok=True)
+
+        with open("dummy/example.txt",'wt') as f:
+            f.write(f"Artifact created at {time.asctime()}")
+        mlflow.log_artifacts("dummy")
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--param1","-p1", type=int, default=5)
+    args.add_argument("--param2","-p2", type=int, default=10)
+    parsed_args = args.parse_args()
+    # parsed_args.param1
+    main(parsed_args.param1, parsed_args.param2)
+    
